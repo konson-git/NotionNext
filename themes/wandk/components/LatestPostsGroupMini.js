be1d710d2276949e9583b03bc63cd6fbbc861796
konson <956093628@qq.com>
2025-05-10 15:15:45 +0800

    config: NEXT_PUBLIC_LINK替换域名

--- themes/wandk/components/LatestPostsGroupMini.js
+++ themes/wandk/components/LatestPostsGroupMini.js
@@ -26,8 +26,7 @@ export default function LatestPostsGroupMini({ latestPosts, siteInfo }) {
         </div>
       </div>
       {latestPosts.map(post => {
-        const selected =
-          currentPath === `${SUB_PATH}/${post.slug}`
+        const selected = currentPath === `${SUB_PATH}/${post.slug}`
         const headerImage = post?.pageCoverThumbnail
           ? post.pageCoverThumbnail
           : siteInfo?.pageCover
@@ -53,7 +52,7 @@ export default function LatestPostsGroupMini({ latestPosts, siteInfo }) {
               }>
               <div>
                 <div className='line-clamp-2 menu-link'>{post.title}</div>
-                <div className='text-gray-400'>{post.lastEditedDay}</div>
+                <div className='text-gray-400'>{post.publishDay}</div>
               </div>
             </div>
           </Link>
